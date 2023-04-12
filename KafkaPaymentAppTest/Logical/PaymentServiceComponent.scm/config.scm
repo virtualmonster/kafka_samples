77f9a06b92d20add547c51394dbad6bf46c12848
Stuart Walker <stuart.walker@hcl.com>
2023-04-12 21:00:03 +0100

    first commit

---
+++ KafkaPaymentAppTest/Logical/PaymentServiceComponent.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1578815991445" createdUser="manoranjansaho" id="7550d8ce:16f98c39cbb:-7ecc" type="service_component_resource" updatedTimestamp="1578815991445" updatedUser="manoranjansaho" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
